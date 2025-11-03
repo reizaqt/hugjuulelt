6dfd5cac02c5ee7c03a93191878ec91c8b8ff2e3
anucat <anucatkk@gmail.com>
2025-11-03 15:18:17 +0800

    "cuspagee salgasan bga hero sectionn"

---
+++ frontend/src/pages/users/customer/components/heroSection.tsx
@@ -0,0 +1,18 @@
+export default function HeroSection() {
+  return (
+    <div className="md:w-1/2 flex flex-col justify-start h-full relative">
+      <div className="relative top-16 left-8 md:top-24 md:left-16">
+        <h1 className="text-4xl md:text-6xl text-amber-700 leading-tight">
+          Get The <br />
+          Smooth Look <br />
+          You Want
+        </h1>
+        <p className="mt-6 text-lg text-amber-700 leading-relaxed max-w-md">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </div>
+  );
+}
